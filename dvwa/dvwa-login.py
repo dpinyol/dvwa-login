64b90bcdb3413a16217c45978991af59333800ac
David Alves <davidpierrealves21@gmail.com>
2022-09-17 18:31:21 -0300

    Update dvwa-login.py

--- dvwa/dvwa-login.py
+++ dvwa/dvwa-login.py
@@ -29,12 +29,7 @@ headers = {
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-User': '?1',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     'dnt': '1',
-    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-gpc': '1',
 }
 
 data = {
